4b9a604ea543a3f6588cce1693ef124bb7d239a1
vskovalev <vairelet@gmail.com>
2023-10-18 08:17:35 +0200

    completed streamlit and viewer

--- Vladislav_Kovalev/utils.py
+++ Vladislav_Kovalev/utils.py
@@ -4,7 +4,34 @@ dataGuestsPath = "./Vladislav_Kovalev/hotels/guests.xlsx"
 dataPreferencesPath = "./Vladislav_Kovalev/hotels/preferences.xlsx"
 
 # output paths
-firstStrategyPath = "./Vladislav_Kovalev/results/first_strategy.xlsx"
-secondStrategyPath = "./Vladislav_Kovalev/results/second_strategy.xlsx"
-thirdStrategyPath = "./Vladislav_Kovalev/results/third_strategy.xlsx"
-fourthStrategyPath = "./Vladislav_Kovalev/results/fourth_strategy.xlsx"
+firstStrategyPath = (
+    "./Vladislav_Kovalev/results_tools/report_display/results/first_strategy.xlsx"
+)
+secondStrategyPath = (
+    "./Vladislav_Kovalev/results_tools/report_display/results/second_strategy.xlsx"
+)
+thirdStrategyPath = (
+    "./Vladislav_Kovalev/results_tools/report_display/results/third_strategy.xlsx"
+)
+fourthStrategyPath = (
+    "./Vladislav_Kovalev/results_tools/report_display/results/fourth_strategy.xlsx"
+)
+
+# viewer paths
+dataHotelsPathWithoutGlobalRepo = "hotels/hotels.xlsx"
+dataGuestsPathWithoutGlobalRepo = "hotels/guests.xlsx"
+dataPreferencesPathWithoutGlobalRepo = "hotels/preferences.xlsx"
+
+# results paths
+firstStrategyPathWithoutGlobalRepo = (
+    "results_tools/report_display/results/first_strategy.xlsx"
+)
+secondStrategyPathWithoutGlobalRepo = (
+    "results_tools/report_display/results/second_strategy.xlsx"
+)
+thirdStrategyPathWithoutGlobalRepo = (
+    "results_tools/report_display/results/third_strategy.xlsx"
+)
+fourthStrategyPathWithoutGlobalRepo = (
+    "results_tools/report_display/results/fourth_strategy.xlsx"
+)
