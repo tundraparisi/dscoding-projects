300b4f6aeb2152352bf3e91652d24b95f0a71eac
AngelicaLongo <angelica.longo@studenti.unimi.it>
2023-10-20 17:13:19 +0200

    data import

--- angelica.longo/angelica_project/angelicOne/angelicone/imported data.py
+++ angelica.longo/angelica_project/angelicOne/angelicone/imported data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-country = pd.read_csv('data/GlobalLandTemperaturesByCountry.csv', sep=",")
-majorcity = pd.read_csv('data/GlobalLandTemperaturesByMajorCity.csv', sep=",")
-state = pd.read_csv('data/GlobalLandTemperaturesByState.csv', sep=",")
+mc = pd.read_csv('data/GlobalLandTemperaturesByMajorCity.csv', sep=",")
+
 
