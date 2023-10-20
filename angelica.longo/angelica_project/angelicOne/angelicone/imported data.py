541ee91d91556e0533354291415b18ffaff61d91
AngelicaLongo <angelica.longo@studenti.unimi.it>
2023-10-20 14:43:16 +0200

    update

---
+++ angelica.longo/angelica_project/angelicOne/angelicone/imported data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+country = pd.read_csv('data/GlobalLandTemperaturesByCountry.csv', sep=",")
+majorcity = pd.read_csv('data/GlobalLandTemperaturesByMajorCity.csv', sep=",")
+state = pd.read_csv('data/GlobalLandTemperaturesByState.csv', sep=",")
+
