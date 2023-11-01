de95e692c1b7753e09501df32a2be874d13c6bd1
ilChurch <stefano.chiesa3@studenti.unimi.it>
2023-11-01 15:09:49 +0100

    date_map_gif() and create_map_range()

--- stefano.chiesa/examProject/functions.py
+++ stefano.chiesa/examProject/functions.py
@@ -1,6 +1,5 @@
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def convert(db, column):
@@ -23,7 +22,7 @@ def convert(db, column):
 
 def create_map(ds, dw):
     # create a world map
-    axis = dw.plot(color='lightblue', edgecolor='black')
+    axis = dw.plot(color='grey', edgecolor='black')
     ds.plot(column='AverageTemperature', ax=axis, markersize=80, legend=True, legend_kwds={'shrink': 0.3})
     plt.title('Average Temperatures in World Major Cities ', fontsize=15)
     fig = plt.gcf()
@@ -41,8 +40,9 @@ def create_map_date(ds, dw, date):
 
 
 def create_map_gif(ds, dw, dates):
+
     fig, ax = plt.subplots(figsize=(20, 16))
-    axis = dw.plot(ax=ax, color='lightblue', edgecolor='black')
+    axis = dw.plot(ax=ax, color='grey', edgecolor='black')
 
     def update(frame):
         current_date = dates[frame]
@@ -60,34 +60,65 @@ def create_map_gif(ds, dw, dates):
         cbar.ax.tick_params(labelsize=14)
         return cbar
 
-    animation = FuncAnimation(fig, update, frames=len(dates), interval=250, init_func= init)
+    animation = FuncAnimation(fig, update, frames=len(dates), interval=250, init_func=init)
     animation.save('temperature_animation.gif', fps=1)
 
 
 def date_map_gif(ds, dw, min_date, max_date):
     fig, ax = plt.subplots(figsize=(20, 16))
-    axis = dw.plot(ax=ax, color='lightblue', edgecolor='black')
-    dates = ds[(ds['dt'] >= min_date) & (ds['dt'] <= max_date)]
+    axis = dw.plot(ax=ax, color='grey', edgecolor='black')
+    panda_dates = ds[(ds['dt'] >= min_date) & (ds['dt'] <= max_date)]
+    dates = panda_dates['dt'].tolist()
 
     def update(frame):
-        try:
-            current_date = dates.iloc[frame]['dt']
-            filtered_cities = ds[ds['dt'] == current_date]
-            scatter = filtered_cities.plot(column='AverageTemperature', ax=axis, markersize=80)
-            plt.title(f'Average Temperatures in World Major Cities ({current_date})', fontsize=25)
-            return scatter
+        current_date = dates[frame]
+        filtered_cities = ds[ds['dt'] == current_date]
+        scatter = filtered_cities.plot(column='AverageTemperature', ax=axis, markersize=80)
+        plt.title(f'Average Temperatures in World Major Cities ({current_date})', fontsize=25)
+        return scatter
 
-        except IndexError:
-            print("Date format not correct or Date non in the dataset")
-            return None  # Return None for frames that caused an index out of range error
+    def init():
+        # Create an empty colorbar with the appropriate colormap and normalization
+        sm = plt.cm.ScalarMappable(cmap='viridis', norm=plt.Normalize(vmin=-40, vmax=40))
+        sm.set_array([])  # empty array for the data range
+        # Add the colorbar to the figure
+        cbar = plt.colorbar(sm, ax=ax, fraction=0.02, pad=0.04)
+        cbar.ax.tick_params(labelsize=14)
+        return cbar
 
-    try:
-        # Create frames using FuncAnimation
-        animation = FuncAnimation(fig, update, frames=len(dates), interval=250)
+    animation = FuncAnimation(fig, update, frames=len(dates), interval=250, init_func=init)
+    animation.save('temperature_animation.gif', fps=1)
 
-        # Add frames to the frames list
-        animation.save('temperature_animation.gif', fps=1)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        # Handle other exceptions if needed
+def create_map_range(ds, dw, start_date, end_date):
+    # Filter data within the specified date range
+    filtered_data = ds[(ds['dt'] >= start_date) & (ds['dt'] <= end_date)]
+
+    # Calculate temperature range for each city within the specified date range
+    city_temperature_ranges = filtered_data.groupby('City')['AverageTemperature'].agg(['min', 'max']).apply(
+        lambda x: (x['min'], x['max']), axis=1)
+
+    # Get top 5 cities with the highest temperature difference within the specified date range
+    top_cities = city_temperature_ranges.apply(lambda x: x[1] - x[0]).nlargest(5).index
+
+    # Filter data for top 5 cities within the specified date range
+    top_cities_data = filtered_data[filtered_data['City'].isin(top_cities)]
+
+    # Create a world map
+    axis = dw.plot(color='grey', edgecolor='black')
+
+    # Plot top 5 cities with the highest temperature range within the specified date range
+    top_cities_data.plot(column='AverageTemperature', ax=axis, markersize=80)
+
+    # Annotate the map with city names and temperature ranges
+    i = -80
+    for idx, (city, (min_temp, max_temp)) in enumerate(city_temperature_ranges[top_cities].items()):
+        plt.text(0, i, f'{city}: {round(min_temp, 1)}°C to {round(max_temp, 1)}°C: {round(max_temp - min_temp, 1)}C°',
+                 fontsize=24, horizontalalignment='center')
+        i = i - 9
+    plt.title('Top 5 Cities with Highest Temperature Range\n{} to {}'.format(start_date, end_date), fontsize=15)
+    fig = plt.gcf()
+    fig.set_size_inches(20, 16)
+    fig.savefig('matplotlib.png', dpi=500, bbox_inches='tight')
+
+    plt.show()
--- stefano.chiesa/examProject/main.py
+++ stefano.chiesa/examProject/main.py
@@ -19,8 +19,8 @@ gpd_world = gpd_world[gpd_world.continent != "Antarctica"]  # removing antartica
 # fun.create_map(gpd_cities, gpd_world)
 
 # fun.create_map_date(gpd_cities, gpd_world, '1990-01-01')
-# dates = gpd_cities['dt'].unique()
-
-#fun.date_map_gif(gpd_cities, gpd_world, '2001-04-01', '2001-12-01')
-
-
+#dates = gpd_cities['dt'].unique()
+#dates = ['2001-04-01', '2001-03-01']
+fun.date_map_gif(gpd_cities, gpd_world, '2001-04-01', '2001-12-01')
+#fun.create_map_gif(gpd_cities, gpd_world, dates)
+#fun.create_map_range(gpd_cities, gpd_world, '1910-08-01', '2005-12-01')
