269d4d486f7371b5c2a5911353b13d11e84c0e2e
chihoang811 <linh.hoang@studenti.unimi.it>
2023-11-04 00:56:15 +0100

    Update function.py

--- chi.hoang/Board_Game_Project/function.py
+++ chi.hoang/Board_Game_Project/function.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib_inline as plt
-
+import matplotlib_pyplot as plt
 
 def Bayesian_average_funct(row):
         n = row['rating_number']
@@ -9,17 +8,7 @@ def Bayesian_average_funct(row):
         bayesian_avg = (prior_weight * prior_mean + n*m) / (prior_weight + n)
         return bayesian_avg
 
-full_table['bayesian_average'] = full_table.apply(lambda row: Bayesian_average_funct(row),axis=1)
 
-
-
-
-plt.hist(full_table['bayesian_average'],
-         bins=20, alpha=0.5
-         label='Bayesian Average')
-plt.xlable('Bayesian Average')
-plt.ylable('Distribution of Bayesian Average Scores')
-plt.legend()
 
 
 
