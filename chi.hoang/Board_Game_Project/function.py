08d588a688dc0acb6db897bb2acd8efe789176ae
chihoang811 <linh.hoang@studenti.unimi.it>
2023-11-05 00:23:10 +0100

    Update function.py

--- chi.hoang/Board_Game_Project/function.py
+++ chi.hoang/Board_Game_Project/function.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import numpy as np
-import matplotlib_pyplot as plt
 
-def Bayesian_average_funct(row):
-        n = row['rating_number']
-        m = row['rating_average']
-        bayesian_avg = (prior_weight * prior_mean + n*m) / (prior_weight + n)
-        return bayesian_avg
+def Bayesian_average_function(row):
+    n = row['rating_number']
+    m = row['rating_average']
+    bayesian_avg = (prior_weight*prior_mean + m*n)/(prior_weight+n)
+    return bayesian_avg
+
+
+
+
+
+
+
+
+
+
+
 
 
 
