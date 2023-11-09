d4598ccd626b040aeec9fbdff7a488db810bc6f7
nikitaolefir <olefirnikita@gmail.com>
2023-11-09 16:39:39 +0100

    changed a docstring for a function

--- Nikita.Olefir/Funcions/dataset_opening.py
+++ Nikita.Olefir/Funcions/dataset_opening.py
@@ -1,16 +1,22 @@
 import pandas as pd
+import os
 
-def open_csv_as_dataframe(file_path:str):
+def open_csv_as_dataframe(file_path: str):
     """ This function opens a csv file as pandas dataframe
 
-    Args:
+    Argumenst:
         file_path (str): the path to the csv file downloaded on a computer
 
     Returns:
        pd.DataFrame: returns a pandas dataframe
+
+    ---
+
+    Examples:
+        open_csv_as_dataframe (/Users/user_name/documents/some_file.csv)
     """    
-    try:
+    if os.path.exists(file_path):
         dataframe = pd.read_csv(file_path)
         return dataframe
-    except Exception as e:
-        print(f"Error reading csv file: {e}")
+    else:
+        return print('File does not exist or the path is incorrect')
