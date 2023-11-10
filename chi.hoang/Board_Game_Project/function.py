58ff615fc93179bd9f7372796b8ab915f931b5dc
chihoang811 <linh.hoang@studenti.unimi.it>
2023-11-10 01:24:49 +0100

    Update function.py

--- chi.hoang/Board_Game_Project/function.py
+++ chi.hoang/Board_Game_Project/function.py
@@ -1,49 +1,20 @@
 import pandas as pd
+from scipy.stats import beta
 
-def Function_Bayesian_Average(row):
+
+def function_bayesian_average(row):
     m = row['rating_average']
     n = row['rating_number']
-    bayesian_avg = (prior_mean*prior_weight + m*n)/(prior_weight+n)
+    bayesian_avg = (prior_mean * prior_weight + m * n) / (prior_weight + n)
     return bayesian_avg
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# beta distribution function to calculate bayesian average
+def calculate_bayesian_average(rating_sum, rating_number, prior_weight, prior_average):
+    alpha = prior_weight + rating_sum
+    beta_ = prior_weight + rating_number - rating_sum
+    mean = beta.stats(alpha, beta_, moments='m')
+    return mean
 
 
 
