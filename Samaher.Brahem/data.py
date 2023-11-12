be226f110a04cbaa92a0b05546929051c259d7cd
Samaher Brahem <samaher.brahem@studenti.unimi.it>
2023-11-12 15:12:57 +0100

    updated on 12/11

--- Samaher.Brahem/data.py
+++ Samaher.Brahem/data.py
@@ -21,21 +21,21 @@ class DataManager:
         self.conn.close()
 
     def get_movie(self):
-        query = "SELECT official_title, year FROM imdb.movie;"
+        query = "SELECT movie.official_title, movie.year FROM imdb.movie LEFT JOIN imdb.rating ON movie.id = rating.movie WHERE rating.votes >0;"
         return pd.DataFrame(self.execute_query(query), columns=['official_title', 'year'])
 
     def get_genre(self):
-        query = "SELECT movie.official_title, genre.genre FROM imdb.genre LEFT JOIN imdb.movie ON movie.id = genre.movie;"
+        query = "SELECT movie.official_title, genre.genre FROM imdb.genre LEFT JOIN imdb.movie ON movie.id = genre.movie LEFT JOIN imdb.rating ON rating.movie = genre.movie WHERE rating.votes >0;"
         return pd.DataFrame(self.execute_query(query), columns=['official_title', 'genre'])
 
     def get_prod(self):
-        query = "SELECT movie.official_title, produced.country FROM imdb.produced LEFT JOIN imdb.movie ON produced.movie = movie.id;"
+        query = "SELECT movie.official_title, produced.country FROM imdb.produced LEFT JOIN imdb.movie ON produced.movie = movie.id LEFT JOIN imdb.rating ON produced.movie = rating.movie WHERE rating.votes >0;"
         return pd.DataFrame(self.execute_query(query), columns=['official_title', 'country'])
 
     def get_rating(self):
-        query = "SELECT movie.official_title, rating.votes, rating.score, rating.scale FROM imdb.movie LEFT JOIN imdb.rating ON movie.id = rating.movie;"
+        query = "SELECT movie.official_title, rating.votes, rating.score, rating.scale FROM imdb.movie LEFT JOIN imdb.rating ON movie.id = rating.movie WHERE rating.votes >0;"
         return pd.DataFrame(self.execute_query(query), columns=['official_title', 'votes', 'score', 'scale'])
 
     def get_crew(self):
-        query = "SELECT movie.official_title, person.given_name, crew.p_role FROM imdb.crew LEFT JOIN imdb.movie ON crew.movie = movie.id LEFT JOIN imdb.person ON crew.person = person.id;"
+        query = "SELECT movie.official_title, person.given_name, crew.p_role FROM imdb.crew LEFT JOIN imdb.movie ON crew.movie = movie.id LEFT JOIN imdb.person ON crew.person = person.id LEFT JOIN imdb.rating ON crew.movie = rating.movie WHERE rating.votes > 0;"
         return pd.DataFrame(self.execute_query(query), columns=['official_title', 'given_name', 'p_role'])
