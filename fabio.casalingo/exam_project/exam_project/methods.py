a50d2d09fae562b19c0d5a1c63a47be92abfa9bb
FabioCasalingo <casalingo.fabio@gmail.com>
2023-11-17 22:41:04 +0100

    update

--- fabio.casalingo/exam_project/exam_project/methods.py
+++ fabio.casalingo/exam_project/exam_project/methods.py
@@ -7,6 +7,9 @@ from geopy.distance import great_circle as gc
 import folium
 import streamlit as st
 import plotly.graph_objects as go
+import plotly.express as px
+from math import radians, sin, cos, sqrt, atan2
+
 
 '''
 Creation of the class, the class is actually one because from my point of view the oop wasn't needed for such type of program
@@ -15,13 +18,56 @@ class Travel:
     '''
     The inizialization of the class requires the path of the dataset, in this case in set a default value for path for
     '''
+
     def __init__(self, dataset_path='C:/Uni/Coding/python/worldcities.xlsx'):
         self.dataset = pd.read_excel(dataset_path)
 
-    '''
-    Given a city ID and a value 'n', this function returns the 'n' closest cities to the input city.
-    '''
+    def haversine_distance(self, coords1, coords2):
+        # Calcola la distanza tra due coordinate utilizzando la formula di Haversine
+        lat1, lon1 = map(radians, coords1)
+        lat2, lon2 = map(radians, coords2)
+
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        # Raggio medio della Terra in chilometri (approssimato)
+        r = 6371.0
+
+        distance = r * c
+        return distance
+
+    # Aggiorna il metodo n_close_city per utilizzare la formula di Haversine
     def n_close_city(self, input_city_id, n):
+        city_coords = (self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0],
+                       self.dataset.loc[self.dataset['id'] == input_city_id, 'lng'].values[0])
+
+        self.dataset['Distance from start'] = self.dataset.apply(
+            lambda row: self.haversine_distance(city_coords, (row['lat'], row['lng'])), axis=1)
+
+        sorted_dataset = self.dataset.sort_values(by='Distance from start')
+
+        closest_cities = sorted_dataset.iloc[1:n]
+        return closest_cities
+
+    '''def n_close_city(self, input_city_id, n):
+        """
+        Calculate the n cities closest to the one given in input.
+
+        Parameters
+        ----------
+        input_city_id: int
+            The ID associated to the starting city.
+        n: int
+            The numbrer of closest cities to select.
+
+        Returns
+        -------
+        closest_cities: list[]
+            A list containaining all the IDs of the n closest cities.
+        """
         city = self.dataset[self.dataset['id'] == input_city_id]
         id_city = int(city['id'].iloc[0])
         city_coords = (self.dataset.loc[self.dataset['id'] == id_city, 'lat'].values[0],
@@ -33,12 +79,24 @@ class Travel:
         sorted_dataset = self.dataset.sort_values(by='Distance from start')
 
         closest_cities = sorted_dataset.iloc[1:n]
-        return closest_cities
+        return closest_cities'''
 
-    '''
-    Given a start city, end city, and a value 'n', this function calculates the shortest path between the two cities.
-    '''
     def distance_between_two_cities(self, start_city, end_city, n):
+        """
+        Calculate the travel path, considering n cities, for going from start_city to end_city .
+
+        Parameters
+        ----------
+        start_city: int
+            The ID associated to the starting city.
+        end_city: int
+            The ID associated to the starting city.
+
+        Returns
+        -------
+        visited_cities: list[]
+            A list containaining all the IDs of the visited cities during the travel.
+        """
         start_coords = self.dataset[(self.dataset['id'] == start_city)][['lat', 'lng']].values[0]
         end_coords = self.dataset[(self.dataset['id'] == end_city)][['lat', 'lng']].values[0]
 
@@ -61,92 +119,107 @@ class Travel:
             visited_cities.append(city)
         return visited_cities
 
-    '''
-     Given a starting city, this function calculates the eastward path until it return to the starting city.
-    '''
     def east(self, input_city_id):
+        """
+        Calculate the travel path, for going from start_city until turning back to it going only to east .
+
+        Parameters
+        ----------
+        input_city_id: int
+            The ID associated to the starting city.
+
+        Returns
+        -------
+        visited_cities: list[]
+            A list containaining all the IDs of the visited cities during the travel.
+        """
         visited_cities = [input_city_id]
         starting_city = input_city_id
-        city_coords = (self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0],
-                       self.dataset.loc[self.dataset['id'] == input_city_id, 'lng'].values[0])
+        original_dataset = self.dataset.copy()
+        city_coords = (
+            self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0],
+            self.dataset.loc[self.dataset['id'] == input_city_id, 'lng'].values[0]
+        )
+        self.dataset['Distance from prev'] = self.dataset.apply(
+            lambda row: gc(city_coords, (row['lat'], row['lng'])).kilometers,
+            axis=1)
+        self.dataset['Diff_lat'] = abs(
+            self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0])
         start_coords = city_coords
         next_city_id = 0
-        x = 0
         while next_city_id != starting_city:
-            self.dataset['Distance from prev'] = self.dataset.apply(lambda row: gc(city_coords, (row['lat'], row['lng'])).kilometers,
-                                                                    axis=1)
-            self.dataset['Distance from start'] = self.dataset.apply(
-                lambda row: gc(start_coords, (row['lat'], row['lng'])).kilometers,
-                axis=1)
-            self.dataset['Diff_lat'] = abs(self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0])
+            east_cities = self.dataset[
+                (self.dataset['lng'] > city_coords[1]) &
+                (abs(self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0]) < 10)
+                ]
 
-            if city_coords[1] > 0:
-                if gc(city_coords, start_coords).kilometers < 150 and len(visited_cities) > 60:
-                    visited_cities.append(starting_city)
-                    next_city_id = starting_city
-                    break
-                east_cities = self.dataset[(self.dataset['lng'] > city_coords[1])]
+            if east_cities.empty:
+                if city_coords[1] < 0:
+                    self.dataset = original_dataset.copy()
+                    self.dataset['Distance from prev'] = self.dataset.apply(
+                        lambda row: gc(city_coords, (row['lat'], row['lng'])).kilometers,
+                        axis=1)
+                    self.dataset['Diff_lat'] = abs(
+                        self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0])
 
-                east_cities = east_cities.sort_values(by='Distance from prev')[:5]
-                if east_cities.empty:
-                    # far partire una diversa logica
-                    westernmost_city = self.dataset[self.dataset['lng'] == self.dataset['lng'].min()]
-                    closest_city = westernmost_city.iloc[0]
+                    east_cities = self.dataset[(self.dataset['lng'] > 0) & (abs(
+                        self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[
+                            0]) < 10)]
+                    self.dataset = self.dataset[self.dataset['lng'].between(city_coords[1], 180)]
                 else:
-                    # Sort the east cities by distance
-                    if (east_cities['lat'].iloc[0], east_cities['lng'].iloc[0]) != start_coords:
-                        east_cities = east_cities.sort_values(by='Diff_lat')
-                    # Get the closest city to the east
-                    closest_city = east_cities.iloc[0]
+                    self.dataset = original_dataset.copy()
+                    self.dataset['Distance from prev'] = self.dataset.apply(
+                        lambda row: gc(city_coords, (row['lat'], row['lng'])).kilometers,
+                        axis=1)
+                    self.dataset['Diff_lat'] = abs(
+                        self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[0])
+
+                    east_cities = self.dataset[(self.dataset['lng'] < 0) & (abs(
+                        self.dataset['lat'] - self.dataset.loc[self.dataset['id'] == input_city_id, 'lat'].values[
+                            0]) < 40)]
+                    self.dataset = self.dataset[self.dataset['lng'].between(-180, 0)]
+                    self.dataset = self.dataset._append(original_dataset[original_dataset['id'] == input_city_id], ignore_index=True)
+
+            east_cities = east_cities.sort_values(by='Distance from prev')[:20]
+            if start_coords[0] > 70 or start_coords[0] < -30:
+                east_cities = east_cities.sort_values(by='Diff_lat')[1:4]
             else:
-                east_cities = self.dataset[(self.dataset['lng'] > city_coords[1]) & (self.dataset['lng'] < 0)]
+                east_cities = east_cities.sort_values(by='Diff_lat')[:4]
 
-                east_cities = east_cities.sort_values(by='Distance from prev')[:5]
+            if not east_cities.empty:
+                next_city_id = east_cities['id'].iloc[0]
+            else:
+                next_city_id = starting_city
 
-                if east_cities.empty:
-                    east_cities = self.dataset[(self.dataset['lng'] > city_coords[1])]
-                    east_cities = east_cities.sort_values(by='Distance from prev')[:5]
-                if (east_cities['lat'].iloc[0], east_cities['lng'].iloc[0]) != start_coords:
-                    east_cities = east_cities.sort_values(by='Distance from start')[:5]
-                closest_city = east_cities.iloc[0]
-
-            next_city_id = closest_city['id']
             visited_cities.append(next_city_id)
-            if -17.4572 <= start_coords[1] <= 1.484 and start_coords[0] < 49.6504 and -60.4467 <= closest_city['lng'] <= -52.7333:
-                city_coords = (self.dataset.loc[self.dataset['id'] == 1620177495, 'lat'].values[0],
-                               self.dataset.loc[self.dataset['id'] == 1620177495, 'lng'].values[0])
-            elif -9.6962 <= start_coords[1] <= -1.9025 and start_coords[0] > 49.6504 and -60.4467 <= closest_city['lng'] <= -52.7333:
-                city_coords = (self.dataset.loc[self.dataset['id'] == 1372403494, 'lat'].values[0],
-                               self.dataset.loc[self.dataset['id'] == 1372403494, 'lng'].values[0])
-            elif 16.5033 <= start_coords[1] <= 49.18 and start_coords[0] < 12.0 and -60.4467 <= closest_city[
-                'lng'] <= -52.7333:
-                visited_cities.append(1076697777)
-                city_coords = (self.dataset.loc[self.dataset['id'] == 1024669127, 'lat'].values[0],
-                               self.dataset.loc[self.dataset['id'] == 1024669127, 'lng'].values[0])
-            elif 92.75 <= start_coords[1] <= 133.6667 and -12.6167 < start_coords[0] < 9.2833 and 79.826 <= \
-                    closest_city['lng'] <= 81.7:
-                city_coords = (self.dataset.loc[self.dataset['id'] == 1360353144, 'lat'].values[0],
-                               self.dataset.loc[self.dataset['id'] == 1360353144, 'lng'].values[0])
-            elif 92.75 <= start_coords[1] <= 133.6667 and start_coords[0] < -12.6167 and 79.826 <= closest_city[
-                'lng'] <= 81.7:
-                city_coords = (self.dataset.loc[self.dataset['id'] == 1036561011, 'lat'].values[0],
-                               self.dataset.loc[self.dataset['id'] == 1036561011, 'lng'].values[0])
-            else:
-                city_coords = (closest_city['lat'], closest_city['lng'])
+            city_coords = (
+                self.dataset.loc[self.dataset['id'] == next_city_id, 'lat'].values[0],
+                self.dataset.loc[self.dataset['id'] == next_city_id, 'lng'].values[0]
+            )
         return visited_cities
 
-    '''
-    Given a list of visited cities, this function calculates the total time of the journey based on distance.
-    '''
     def time(self, visited):
+        """
+        Calculate the time give a travel path.
+
+        Parameters
+        ----------
+        visited: list[]
+            The IDs associated to a path.
+
+        Returns
+        -------
+        duration: int
+           The time needed to complete the travel path given as input.
+        """
         duration = 0
         for i in range(len(visited) - 1):
             a_coords = (
-            self.dataset.loc[self.dataset['id'] == visited[i], 'lat'].values[0],
-            self.dataset.loc[self.dataset['id'] == visited[i], 'lng'].values[0])
+                self.dataset.loc[self.dataset['id'] == visited[i], 'lat'].values[0],
+                self.dataset.loc[self.dataset['id'] == visited[i], 'lng'].values[0])
             b_coords = (
-            self.dataset.loc[self.dataset['id'] == visited[i + 1], 'lat'].values[0],
-            self.dataset.loc[self.dataset['id'] == visited[i + 1], 'lng'].values[0])
+                self.dataset.loc[self.dataset['id'] == visited[i + 1], 'lat'].values[0],
+                self.dataset.loc[self.dataset['id'] == visited[i + 1], 'lng'].values[0])
             if gc(a_coords, b_coords).kilometers <= 1000:
                 duration += 4
             elif 1000 < gc(a_coords, b_coords).kilometers <= 2000:
@@ -155,18 +228,32 @@ class Travel:
                 duration += 8
         return duration
 
-    '''
-    Given a list of visited cities, this function creates a 2D map with markers and a line connecting them.
-    '''
+class MapHandler:
+    def __init__(self, dataset_path='C:/Uni/Coding/python/worldcities.xlsx'):
+        self.dataset = pd.read_excel(dataset_path)
+
     def map_2d(self, visit):
+        """
+        Show with a 2d map the path of the input travel path.
+
+        Parameters
+        ----------
+        visit: list[]
+            The IDs associated to the cities visited in the travel path.
+
+        Returns
+        -------
+        map: folium.folium.Map
+            A folium map containing the graphical travel path.
+        """
         zoom = self.dataset[(self.dataset['id'] == visit[0])][['lat', 'lng']].values[0]
-        mappa = folium.Map(location=zoom, zoom_start=2)
+        map = folium.Map(location=zoom, zoom_start=2)
         icon_url = 'C:/Uni/Coding/python/exam_project/Map-Marker-Free-Download-PNG.png'
-        folium.TileLayer('cartodbdark_matter').add_to(mappa)
+        folium.TileLayer('cartodbdark_matter').add_to(map)
         for i in range(len(visit)):
             s = self.dataset[(self.dataset['id'] == visit[i])][['lat', 'lng']].values[0]
             folium.Marker(s, icon=folium.CustomIcon(icon_image=icon_url, icon_size=(25, 25)),
-                          popup=self.dataset[(self.dataset['id'] == visit[i])][['city']].values[0]).add_to(mappa)
+                          popup=self.dataset[(self.dataset['id'] == visit[i])][['city']].values[0]).add_to(map)
         locations = [(self.dataset.loc[self.dataset['id'] == visit[j], ['lat', 'lng']].values[0]) for j in
                      range(len(visit))]
         line = folium.PolyLine(locations=locations,
@@ -174,16 +261,26 @@ class Travel:
                                weight=6,
                                opacity=0.3
                                )
-        line.add_to(mappa)
-        mappa.save('mappa.html')
-        return mappa
+        line.add_to(map)
+        map.save('mappa.html')
+        return map
 
-    '''
-    Given a list of visited cities, this function creates a 3D map with lines connecting them on a globe.
-    '''
     def map_3d(self, visit):
+        """
+        Show with a 3d map the path of the input travel path.
 
-        coordinates = [self.dataset.loc[self.dataset['id'] == city_id, ['lat', 'lng', 'city']].iloc[0] for city_id in visit]
+        Parameters
+        ----------
+        visit: list[]
+            The IDs associated to the cities visited in the travel path.
+
+        Returns
+        -------
+        fig: plotly.graph_objs.Figure
+            A 3d map that show the travel path.
+        """
+        coordinates = [self.dataset.loc[self.dataset['id'] == city_id, ['lat', 'lng', 'city']].iloc[0] for city_id in
+                       visit]
 
         path = go.Scattergeo(
             lon=[coord['lng'] for coord in coordinates],
@@ -191,7 +288,8 @@ class Travel:
             mode="lines+markers",
             line=dict(width=2, color="black"),
             marker=dict(size=8,
-            color=["orange" if i == 0 else "red" if i == len(coordinates) - 1 else " #ffb84d" for i in range(len(coordinates))]),
+                        color=["orange" if i == 0 else "red" if i == len(coordinates) - 1 else " #ffb84d" for i in
+                               range(len(coordinates))]),
             text=[f"City: {coord['city']}" for coord in coordinates]
         )
 
@@ -203,7 +301,7 @@ class Travel:
                 countrycolor="rgb(0, 0, 0)",
                 oceancolor="#80bfff",
                 projection=dict(
-                type="orthographic"
+                    type="orthographic"
                 )
             )
         )
@@ -220,3 +318,21 @@ class Travel:
         fig.update_layout(height=800, margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
         fig.show()
+        return fig
+
+    def population(self,dataset):
+        df = dataset.groupby('iso3')['population'].sum().reset_index()
+        df
+        fig = px.choropleth(df,
+                            locations='iso3',
+                            color='population',
+                            hover_name='iso3',
+                            projection='natural earth',
+                            range_color=(0, 155000000),
+                            title='Population distribution',
+                            color_continuous_scale='Viridis')
+
+        fig.show()
+
+
+
