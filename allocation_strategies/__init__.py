6a276ca0364ce63af33c261d3e519c268f7db9d0
tundra-parisi <tundrabtc@gmail.com>
2023-11-19 14:10:44 +0100

    Upload

---
+++ allocation_strategies/__init__.py
@@ -0,0 +1,8 @@
+# qui posso mettere hello world per farlo poi eseguire in un'altra directory
+# lui dentro forces ha una classe con dentro una funzione, poi va dentro __init__
+# from .forces import Forces
+# o anche from physics import Forces (inteso come Forces il file)
+
+from .random_allocation import Hello
+
+Hello()
